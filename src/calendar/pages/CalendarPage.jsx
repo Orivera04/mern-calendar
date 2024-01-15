f8968f0045df118950baaf41c0444ad97be6aa0c
orivera04 <riveramoreira04@gmail.com>
2024-01-15 10:08:26 -0600

    Feat: Add events to calendar.

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,78 @@
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours, set } from 'date-fns';
 import { NavBar } from "../components/NavBar"
+import { localizer } from '../../helpers/calendarLocalizer';
+import { getMessagesES } from '../../helpers';
+import { CalendarEvent } from '../components/CalendarEvent';
+import { useState } from 'react';
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgcolor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Oscar'
+  }
+}];
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'week' );
+
+  const onDoubleClick = ( event ) => {
+    console.log({ onDoubleClick: event });
+  };
+
+  const onSelect = ( event ) => {
+    console.log({ onSelect: event });
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+    setLastView(event);
+  }
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <>
       <NavBar />
+
+      <Calendar
+        culture='es'
+        localizer={ localizer }
+        events={ events }
+        defaultView= { lastView }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+
+        }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChanged }
+      />
+
     </>
   )
 }
